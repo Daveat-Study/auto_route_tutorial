fa38290d107c6b3f5a45c1e31b8a2431ba5948e1
Seyhar Von <seyhavon22@gmail.com>
2024-03-16 14:37:54 +0700

    feat(auto_route_tutorial): initialize project

---
+++ lib/routers/router.gr.dart
@@ -0,0 +1,69 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:auto_route_tutorial/main.dart' as _i1;
+import 'package:flutter/material.dart' as _i3;
+
+abstract class $AppRouter extends _i2.RootStackRouter {
+  $AppRouter({super.navigatorKey});
+
+  @override
+  final Map<String, _i2.PageFactory> pagesMap = {
+    MyHomeRoute.name: (routeData) {
+      final args = routeData.argsAs<MyHomeRouteArgs>();
+      return _i2.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: _i1.MyHomePage(
+          key: args.key,
+          title: args.title,
+        ),
+      );
+    }
+  };
+}
+
+/// generated route for
+/// [_i1.MyHomePage]
+class MyHomeRoute extends _i2.PageRouteInfo<MyHomeRouteArgs> {
+  MyHomeRoute({
+    _i3.Key? key,
+    required String title,
+    List<_i2.PageRouteInfo>? children,
+  }) : super(
+          MyHomeRoute.name,
+          args: MyHomeRouteArgs(
+            key: key,
+            title: title,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'MyHomeRoute';
+
+  static const _i2.PageInfo<MyHomeRouteArgs> page =
+      _i2.PageInfo<MyHomeRouteArgs>(name);
+}
+
+class MyHomeRouteArgs {
+  const MyHomeRouteArgs({
+    this.key,
+    required this.title,
+  });
+
+  final _i3.Key? key;
+
+  final String title;
+
+  @override
+  String toString() {
+    return 'MyHomeRouteArgs{key: $key, title: $title}';
+  }
+}
